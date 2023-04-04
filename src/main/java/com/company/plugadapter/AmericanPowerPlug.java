6c6cbf8d612afe5207853d7a8895cb5bdd389936
chrisMasoud <chrismasoud22@gmail.com>
2023-04-04 08:10:13 -0400

    Created AmericanPowerPlug class

---
+++ src/main/java/com/company/plugadapter/AmericanPowerPlug.java
@@ -0,0 +1,7 @@
+package com.company.plugadapter;
+
+public class AmericanPowerPlug implements AmericanPlug {
+    public void plugIntoAmericanSocket() {
+        System.out.println("American plug is plugged into an American socket.");
+    }
+}
